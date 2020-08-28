7b297a27a6292176f7feb7963975a3fd22b868b6
Ackerr <wzmmmmj@gmail.com>
2020-08-28 11:18:34 +0800

    feat: support local fzf command

--- internal/fzf.go
+++ internal/fzf.go
@@ -2,8 +2,12 @@ package internal
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/ackerr/lab/utils"
 	"github.com/ktr0731/go-fuzzyfinder"
@@ -30,9 +34,47 @@ func readLines(filePath string) (lines []string, err error) {
 func Fuzzy(filePath string) string {
 	lines, err := readLines(filePath)
 	utils.Check(err)
-	index, err := fuzzyfinder.Find(lines, func(i int) string {
-		return lines[i]
-	})
-	utils.Check(err)
-	return lines[index]
+	filtered := fuzzyFinder(lines)
+	return filtered
+}
+
+func fuzzyFinder(lines []string) (filtered string) {
+	if checkCommandExists("fzf") {
+		filters := withFilter("fzf", func(in io.WriteCloser) {
+			for _, line := range lines {
+				fmt.Fprintln(in, line)
+				time.Sleep(5 * time.Millisecond)
+			}
+		})
+		fmt.Println(filters)
+		filtered = filters[0]
+	} else {
+		index, err := fuzzyfinder.Find(lines, func(i int) string {
+			return lines[i]
+		})
+		utils.Check(err)
+		filtered = lines[index]
+	}
+	return
+}
+
+func checkCommandExists(command string) bool {
+	_, err := exec.LookPath(command)
+	return err != nil
+}
+
+func withFilter(command string, input func(in io.WriteCloser)) []string {
+	shell := os.Getenv("SHELL")
+	if len(shell) == 0 {
+		shell = "sh"
+	}
+	cmd := exec.Command(shell, "-c", command)
+	cmd.Stderr = os.Stderr
+	in, _ := cmd.StdinPipe()
+	go func() {
+		input(in)
+		in.Close()
+	}()
+	result, _ := cmd.Output()
+	return strings.Split(string(result), "\n")
 }
