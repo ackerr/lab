c98de30df5a88ebb6a947004f34646bc56bf6fa2
Ackerr <wzmmmmj@gmail.com>
2020-09-28 19:31:06 +0800

    feat: lab open support subpage

--- cmd/open.go
+++ cmd/open.go
@@ -1,19 +1,28 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/ackerr/lab/internal"
 	"github.com/ackerr/lab/utils"
 	"github.com/spf13/cobra"
 )
 
+var subpage string
+
+func init() {
+	openCmd.Flags().BoolP("pipelines", "p", false, "open pipeline page")
+	openCmd.Flags().BoolP("merge_requests", "m", false, "open merge_requests page")
+	openCmd.Flags().StringVar(&subpage, "subpage", "", "open the input subpage")
+}
+
 var openCmd = &cobra.Command{
 	Use:   "open [remote]",
-	Short: "Open the current repo in web browser",
+	Short: "Open the current repo in web browser, like <lab open> or <lab open origin>",
 	Run:   openCurrentRepo,
 }
 
 func openCurrentRepo(cmd *cobra.Command, args []string) {
-	// check the path in a git repo
 	var remote string
 	if len(args) > 0 {
 		remote = args[0]
@@ -28,6 +37,22 @@ func openCurrentRepo(cmd *cobra.Command, args []string) {
 	}
 	gitURL := internal.RemoteURL(remote)
 	url := internal.TransferGitURLToURL(gitURL)
+
+	if len(subpage) == 0 {
+		isPL, _ := cmd.Flags().GetBool("pipelines")
+		if isPL {
+			subpage = "pipelines"
+		}
+	}
+	if len(subpage) == 0 {
+		isMR, _ := cmd.Flags().GetBool("merge_requests")
+		if isMR {
+			subpage = "merge_requests"
+		}
+	}
+	if len(subpage) > 0 {
+		url = fmt.Sprintf("%s/-/%s", url, subpage)
+	}
 	err = utils.OpenBrowser(url)
 	utils.Check(err)
 }
