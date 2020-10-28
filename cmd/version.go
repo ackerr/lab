6ef811856455ec1064e2290164be0d5093143a3d
Ackerr <wzmmmmj@gmail.com>
2020-10-28 14:17:33 +0800

    bump version v0.2.1 -> v0.2.2

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const version = "v0.2.2"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Lab",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lab v0.2.1")
+		fmt.Printf("lab %s\n", version)
 	},
 }
 
