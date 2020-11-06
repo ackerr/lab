fddda63a42588025f428f3ae8462e52b64d1b998
Ackerr <wzmmmmj@gmail.com>
2020-11-06 10:59:11 +0800

    Bump version: 0.2.8 → 0.2.9

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.2.8
+VERSION=0.2.9
 
 BIN=lab
 DIST_DIR=dist
--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "v0.2.8"
+const version = "v0.2.9"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
